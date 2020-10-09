6424493e9adf960e444fcb7841d1e5fba65481b2
DmanCoder <ktdenis.1@live.com>
2020-10-09 14:54:09 +1000

    Added navigation

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Hamburger = () => {
-  return <div className=''>menu</div>;
+  return (
+    <div className="hamburger-menu">
+      <div className="menu-secondary-background-color"></div>
+      <div className="menu-layer">
+        <div className="menu-city-background"></div>
+        <div className="container">
+          <div className="wrapper">
+            <div className="menu-links">
+              <nav>
+                <ul>
+                  <li>
+                    <Link to="/opportunities">Opportunities</Link>
+                  </li>
+                  <li>
+                    <Link to="/solutions">Solutions</Link>
+                  </li>
+                  <li>
+                    <Link to="/contact-us">Contact Us</Link>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hamburger;
